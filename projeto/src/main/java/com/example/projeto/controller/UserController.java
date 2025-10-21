989d1c8e768442603f88b3cb2bb6c4c5c1857b0b
João Pedro <jpbs@cesar.school>
2025-10-21 10:24:33 -0300

    Change response to http instead of string

--- projeto/src/main/java/com/example/projeto/controller/UserController.java
+++ projeto/src/main/java/com/example/projeto/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -22,32 +22,30 @@ public class UserController {
 //        this.userService = userService;
 //    }
 
-    @GetMapping("/users")
+    @GetMapping()
     public List<UserModel> listarUsuarios(){
         return userService.listarUsuarios();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserModel> buscarUsuarioPorId(@PathVariable Long id){
         //Entender esse return
         return userService.buscarPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/users")
+    @PostMapping()
     public UserModel adicionarUsuario(@RequestBody UserModel user){
         return userService.inserirUsuario(user);
     }
 
-    @PutMapping("/users")
+    @PutMapping()
     public Optional<UserModel> atualizarUsuario(@RequestBody UserModel user){
         return userService.atualizarEmail(user.getId(), user.getEmail());
     }
 
-    @DeleteMapping("/users/{id}")
-    public String deletarUsuario(@PathVariable Long id){
-        if (userService.deletarUsuario(id).isEmpty()){
-            return "Usuario Nao Encontrado";
-        }
-        return "Usuario Deletado com Sucesso";
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id){
+        userService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
     }
 }
