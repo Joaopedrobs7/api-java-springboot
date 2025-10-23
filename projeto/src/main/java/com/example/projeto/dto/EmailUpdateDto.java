b306bf4bc569ba7e9603ccf450c3b121d82499ed
João Pedro <jpbs@cesar.school>
2025-10-23 09:16:36 -0300

    Added Validation Constraints on EmailUpdateDto

--- projeto/src/main/java/com/example/projeto/dto/EmailUpdateDto.java
+++ projeto/src/main/java/com/example/projeto/dto/EmailUpdateDto.java
@@ -1,8 +1,11 @@
 package com.example.projeto.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class EmailUpdateDto {
+
+    @Email(message = "Email invalido")
     private String email;
 }
