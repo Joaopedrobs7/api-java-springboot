49fee837f11d0e2d3f6bad4ea8c3cf433b8cd34b
João Pedro <jpbs@cesar.school>
2025-10-23 09:18:33 -0300

    Added Validation Constraints on UserModelDto

--- projeto/src/main/java/com/example/projeto/dto/UserModelDto.java
+++ projeto/src/main/java/com/example/projeto/dto/UserModelDto.java
@@ -1,9 +1,14 @@
 package com.example.projeto.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserModelDto {
+    @Size(min = 3, max = 100, message = "Nome invalido")
     private String name;
+
+    @Email(message = "Email invalido")
     private String email;
 }
