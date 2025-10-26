f0e75796580089c443f4da4c7c168767be8eb796
joaop <jpbs@cesar.school>
2025-10-25 22:44:02 -0300

    Create GlobalExceptionHandler

--- projeto/src/main/java/com/example/projeto/handler/GlobalExceptionHandler.java
+++ projeto/src/main/java/com/example/projeto/handler/GlobalExceptionHandler.java
@@ -1,50 +1,25 @@
 package com.example.projeto.handler;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    // --- Tratamento de Erro de Validação (400 Bad Request) ---
-    // Você não precisa mais do @ResponseStatus, pois o Spring lida com o 400
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
 
-    // --- Tratamento de Recurso Não Encontrado (404 Not Found) ---
-    @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND) // Altere o status para 404
-    public Map<String, String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        Map<String, String> errors = new HashMap<>();
-        // Boas práticas: use uma chave genérica para erros de recurso
-        errors.put("resource", ex.getMessage());
-        return errors;
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleNoSuchElementException(NoSuchElementException ex) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("resource", "Usuario não encontrado");
-        return errors;
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
