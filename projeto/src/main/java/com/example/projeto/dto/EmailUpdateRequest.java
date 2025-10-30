8fdeaad467bb2f011d7666b66b701224a92b1ec7
João Pedro <jpbs@cesar.school>
2025-10-30 20:48:09 -0300

    refactor(api): make exceptions more user-friendly

--- projeto/src/main/java/com/example/projeto/dto/EmailUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.projeto.dto;
-
-import jakarta.validation.constraints.Email;
-
-public record EmailUpdateRequest(
-        @Email(message = "Email invalido")
-        String email
-) {
-}
---
+++ projeto/src/main/java/com/example/projeto/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.example.projeto.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleUserNotFound(Exception ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleBadFormatUser(MethodArgumentNotValidException ex) {
+
+        // 1. Mapa para guardar os erros de campo
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        // 2. Itera sobre todos os erros contidos na exceção
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            fieldErrors.put(fieldName, errorMessage);
+        });
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("error", "Requisição Inválida (Validação de Campos)");
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("details", fieldErrors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
